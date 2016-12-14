d500158d1f4470a6908fdbf93f6de5c80834605d
Daniel Cordero <github@xxoo.ws>
2016-12-14 09:40:37 +0000

    LH L2 toggle -> Tab

--- keyboard/ergodox_ez/keymaps/ben/keymap.c
+++ keyboard/ergodox_ez/keymaps/ben/keymap.c
@@ -19,16 +19,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  Y   |           |  L2  |   Y  |   U  |   I  |   O  |   P  | Del    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ;/:  | Enter  |
- * |--------+------+------+------+------+------|  L2  |           |  L1  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Tab  |           |  L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  | ,/<  |  ./> | //?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  ~L1 | \/|  | Cmd  | Alt  | Ctrl |                                       |   B  | '/@  | #/~  | Alt  | Ctrl   |
  *   `----------------------------------'                                       `------------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | Home | End  |       | Left | Right|
+ *                                        ,-------------.       ,---------------.
+ *                                        | Home | End  |       | Left |  Right |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | PgUp |       | Up   |        |      |
- *                                 | Space| Shift|------|       |------|  Bksp  | Space|
+ *                                 | Space|LShift|------|       |------|  Bksp  | Space|
  *                                 |      |      | PgDn |       | Down |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,    KC_3,     KC_4,            KC_5,     KC_6,
         KC_TAB,         KC_Q,         KC_W,    KC_E,     KC_R,            KC_T,     KC_Y,
         KC_TAB,         KC_A,         KC_S,    KC_D,     KC_F,            KC_G,
-        KC_LSFT,        KC_Z,         KC_X,    KC_C,     KC_V,            KC_B,     TG(MDIA),
+        KC_LSFT,        KC_Z,         KC_X,    KC_C,     KC_V,            KC_B,     KC_TAB,
         MO(SYMB),       UK_BSLS,      KC_LGUI, KC_LALT,  KC_LCTL,
                                       // Thumb cluster
                                                KC_HOME,  KC_END,
